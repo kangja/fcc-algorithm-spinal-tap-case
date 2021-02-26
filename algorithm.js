4cd00a2f5748a136e45add939c4f2e465c5e98e4
Jason <kangja1996@gmail.com>
2021-02-26 12:08:29 -0500

    progress

---
+++ algorithm.js
@@ -0,0 +1,21 @@
+function pairElement(str) {
+  const output = [];
+
+  for (const c of str) {
+    if (c === "A") {
+      output.push([c, "T"]);
+    } else if (c === "T") {
+      output.push([c, "A"]);
+    } else if (c === "C") {
+      output.push([c, "G"]);
+    } else if (c === "G") {
+      output.push([c, "C"]);
+    }
+  }
+  return output;
+}
+
+console.log(pairElement("CTCTA"));
+
+
+
